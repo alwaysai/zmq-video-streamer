a5cd86b06b24ef1f90f6fac226eff74e0140e55d
Eric VanBuhler <eric@alwaysai.co>
2021-02-03 17:22:40 -0800

    Use flask to display images on browser window

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
-from flask_socketio import SocketIO
-from flask import Flask, render_template, request
+from flask import Flask, render_template, Response
+import cv2
+import imagezmq
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -11,31 +11,29 @@ def index():
     return render_template('index.html')
 
 
-@socketio.on('connect', namespace='/web')
-def connect_web():
-    print('[INFO] Web client connected: {}'.format(request.sid))
+def gen_video_feed():
+    while True:
+        img_hub = app.config['IMAGE_HUB']
+        text, image = img_hub.recv_image()
+
+        # Encode image as jpeg
+        image = cv2.imencode(
+                '.jpg', image,
+                [int(cv2.IMWRITE_JPEG_QUALITY), 75])[1].tobytes()
+        yield (
+                b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
 
 
-@socketio.on('disconnect', namespace='/web')
-def disconnect_web():
-    print('[INFO] Web client disconnected: {}'.format(request.sid))
-
-
-@socketio.on('connect', namespace='/cv')
-def connect_cv():
-    print('[INFO] CV client connected: {}'.format(request.sid))
-
-
-@socketio.on('disconnect', namespace='/cv')
-def disconnect_cv():
-    print('[INFO] CV client disconnected: {}'.format(request.sid))
-
-
-@socketio.on('cv2server')
-def handle_cv_message(message):
-    socketio.emit('server2web', message, namespace='/web')
+@app.route('/video')
+def video():
+    return Response(
+            gen_video_feed(),
+            mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == "__main__":
-    print('[INFO] Starting server at http://localhost:5001')
-    socketio.run(app=app, host='0.0.0.0', port=5001)
+    print('[INFO] Starting server at http://localhost:5000')
+    app.config['IMAGE_HUB'] = imagezmq.ImageHub(
+            open_port='tcp://127.0.0.1:5001', REQ_REP=False)
+    app.run(host='0.0.0.0', port=5000)
