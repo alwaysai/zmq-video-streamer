5168effb45bf2c7f260c60bee66b8c7ea8ee70a2
Eric VanBuhler <eric@alwaysai.co>
2021-02-03 16:58:41 -0800

    Update client to use imagezmq

--- cv/app.py
+++ cv/app.py
@@ -1,87 +1,37 @@
 import time
 import edgeiq
 import argparse
-import socketio
 import cv2
 import base64
-"""
-Use object detection to detect objects in the frame in realtime. The
-types of objects detected can be changed by selecting different models.
-
-To change the computer vision model, follow this guide:
-https://dashboard.alwaysai.co/docs/application_development/changing_the_model.html
-
-To change the engine and accelerator, follow this guide:
-https://dashboard.alwaysai.co/docs/application_development/changing_the_engine_and_accelerator.html
-"""
-
-sio = socketio.Client()
-
-
-@sio.event
-def connect():
-    print('[INFO] Successfully connected to server.')
-
-
-@sio.event
-def connect_error():
-    print('[INFO] Failed to connect to server.')
-
-
-@sio.event
-def disconnect():
-    print('[INFO] Disconnected from server.')
+import imagezmq
 
 
 class CVClient(object):
-    def __init__(self, server_addr, stream_fps):
+    def __init__(self, server_addr):
         self.server_addr = server_addr
         self.server_port = 5001
-        self._stream_fps = stream_fps
-        self._last_update_t = time.time()
-        self._wait_t = (1/self._stream_fps)
 
     def setup(self):
-        print('[INFO] Connecting to server http://{}:{}...'.format(
+        print('[INFO] Connecting to server tcp://{}:{}...'.format(
             self.server_addr, self.server_port))
-        sio.connect(
-                'http://{}:{}'.format(self.server_addr, self.server_port),
-                transports=['websocket'],
-                namespaces=['/cv'])
+        self._sender = imagezmq.ImageSender(
+                connect_to='tcp://{}:{}'.format(self.server_addr, self.server_port),
+                REQ_REP=False)
         time.sleep(1)
         return self
 
-    def _convert_image_to_jpeg(self, image):
-        # Encode frame as jpeg
-        frame = cv2.imencode('.jpg', image)[1].tobytes()
-        # Encode frame in base64 representation and remove
-        # utf-8 encoding
-        frame = base64.b64encode(frame).decode('utf-8')
-        return "data:image/jpeg;base64,{}".format(frame)
-
     def send_data(self, frame, text):
-        cur_t = time.time()
-        if cur_t - self._last_update_t > self._wait_t:
-            self._last_update_t = cur_t
-            frame = edgeiq.resize(
-                    frame, width=640, height=480, keep_scale=True)
-            sio.emit(
-                    'cv2server',
-                    {
-                        'image': self._convert_image_to_jpeg(frame),
-                        'text': '<br />'.join(text)
-                    })
+        self._sender.send_image('test', frame)
 
     def check_exit(self):
         pass
 
     def close(self):
-        sio.disconnect()
+        pass
 
 
-def main(camera, use_streamer, server_addr, stream_fps):
-    obj_detect = edgeiq.ObjectDetection(
-            "alwaysai/mobilenet_ssd")
+def main(camera, use_streamer, server_addr):
+    obj_detect = edgeiq.ObjectDetection("alwaysai/mobilenet_ssd")
     obj_detect.load(engine=edgeiq.Engine.DNN)
 
     print("Loaded model:\n{}\n".format(obj_detect.model_id))
@@ -96,7 +46,7 @@ def main(camera, use_streamer, server_addr, stream_fps):
         if use_streamer:
             streamer = edgeiq.Streamer().setup()
         else:
-            streamer = CVClient(server_addr, stream_fps).setup()
+            streamer = CVClient(server_addr).setup()
 
         with edgeiq.WebcamVideoStream(cam=camera) as video_stream:
             # Allow Webcam to warm up
@@ -138,7 +88,7 @@ def main(camera, use_streamer, server_addr, stream_fps):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='alwaysAI Video Streamer')
+    parser = argparse.ArgumentParser(description='alwaysAI ZMQ Video Streamer Client')
     parser.add_argument(
             '--camera', type=int, default='0',
             help='The camera index to stream from.')
@@ -148,8 +98,5 @@ if __name__ == "__main__":
     parser.add_argument(
             '--server-addr',  type=str, default='localhost',
             help='The IP address or hostname of the SocketIO server.')
-    parser.add_argument(
-            '--stream-fps',  type=float, default=20.0,
-            help='The rate to send frames to the server.')
     args = parser.parse_args()
-    main(args.camera, args.use_streamer, args.server_addr, args.stream_fps)
+    main(args.camera, args.use_streamer, args.server_addr)
